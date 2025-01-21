43c6f273f40acc2752ed07bd875a7bbc855bdc5a
manel <manelocana.dev@gmail.com>
2025-01-21 18:21:36 +0100

    ejercicio diccionario

--- aprendeconALF_2atanda.py
+++ aprendeconALF_2atanda.py
@@ -1053,15 +1053,16 @@ print(f'{kilos} kg de {fruta} valen {precio:.2f} €' if fruta in frutas else 'n
 # Escribir un programa que pregunte una fecha en formato dd/mm/aaaa y muestre por pantalla la misma fecha en 
 # formato dd de <mes> de aaaa donde <mes> es el nombre del mes.
 
+def fecha():
+    fecha = input('dame una fecha en formato dd/mm/aaaa: ')
+    mes =  {
+        1:'enero', 2:'febrero', 3:'marzo', 4:'abril', 5:'mayo', 6:'junio', 7:'julio', 8:'agosto', 9:'septiembre',
+        10:'octubre', 11:'noviembre', 12:'diciembre'
+    }
 
-
-#fecha = input('dame una fecha en formato dd/mm/aaaa')
-mes =  {
-    'enero':1, 'febrero':2, 'marzo':3, 'abril':4, 'mayo':5, 'junio':6, 'julio':7, 'agosto':8, 'septiembre':9,
-    'octubre':10, 'noviembre':11, 'diciembre':12
-}
-#print(f'{fecha[]}')
-
+    fecha_separada = fecha.split('/')
+    print(f'{fecha_separada[0]} de {mes[int(fecha_separada[1])]} de {fecha_separada[2]}')
+fecha()
 
 
 # Ejercicio 5
